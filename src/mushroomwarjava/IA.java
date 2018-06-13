56288c92774dbaf89d7ccf9b20bb66592027d2f5
GermainJonathan <jgermain@gismartware.com>
2018-06-13 23:49:49 +0200

    :pencil2: Commentaire commentaire

--- src/mushroomwarjava/IA.java
+++ src/mushroomwarjava/IA.java
@@ -9,29 +9,57 @@ import component.House;
 import java.awt.Point;
 
 /**
- *
+ * Intelligence Artificiel du jeu
  * @author Jonathan
  */
 public class IA extends Thread {
     
+    /**
+     * Joueur factice rataché à cette IA
+     */
     private botIA bot;
+    
+    /**
+     * instance du jeu en cours
+     */
     private gameUI UI;
+    
+    /**
+     * boolean permetant d'arrêter ou non le thread
+     */
     private boolean activeIA;
     
+    /**
+     * constructeur
+     * @param bot
+     * @param game 
+     */
     public IA(botIA bot, gameUI game) {
         this.activeIA = true;
         this.UI = game;
         this.bot = bot;
     }
 
+    /**
+     * Attaque d'une autre maison
+     * @param source
+     * @param target 
+     */
     private void attackNeighbor(House source, House target) {
         target.isTarget(source);
     }
     
+    /**
+     * Permet d'arrêter le thread
+     */
     public void stopIA(){
         this.activeIA = false;
     }
     
+    /**
+     * Action du thread
+     * Une attaque sera lancée si une des maisons appartenant au bot possède plus de 15 unitées
+     */
     @Override
     public void run() {
         while(this.bot.isAlive() && this.activeIA) {
--- src/mushroomwarjava/Player.java
+++ src/mushroomwarjava/Player.java
@@ -10,25 +10,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Joueur
  * @author Jonathan Germain
  */
 public class Player {
     
+    /**
+     * Constante permetant la définition des équipes
+     * 0 pour l'équipe Rouge
+     * 1 pour l'équipe Bleu
+     */
     public static int TEAM_RED = 0;
+    
+    /**
+     * Constante permetant la définition des équipes
+     * 0 pour l'équipe Rouge
+     * 1 pour l'équipe Bleu
+     */
     public static int TEAM_BLUE = 1;
+    
+    /**
+     * Equipe du joueur
+     */
     protected final int team;
+    
+    /**
+     * Nom du joueur
+     */
     protected final String name;
     
+    /**
+     * Liste des maisons lui appartenant
+     */
     protected List<House> houses;
+    
+    /**
+     * Maison selectionné par le joueur
+     */
     protected House selectedHouse;
 
+    /**
+     * Constructeur
+     * @param team
+     * @param name 
+     */
     public Player(int team, String name) {
         this.houses = new ArrayList<>();
         this.team = team;
         this.name = name;
     }
     
+    /**
+     * Retourne le nombre total d'unité du joueur
+     * @return 
+     */
     public int getUnities() {
         int compt = 0;
         for(House elem : houses) {
@@ -37,22 +72,42 @@ public class Player {
         return compt;
     }
 
+    /**
+     * Retourne l'équipe du joueur
+     * @return 
+     */
     public int getTeam() {
         return team;
     }
 
+    /**
+     * Retourne le nom du joueur
+     * @return 
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Permet d'ajouter une maison à la liste 
+     * @param theMaison 
+     */
     public void addHouse(House theMaison) {
         this.houses.add(theMaison);
     }
     
+    /**
+     * Retourne la maison sélectionné par le joueur
+     * @return 
+     */
     public House getSelectedHouse() {
         return this.selectedHouse;
     }
     
+    /**
+     * Selection de la maison active
+     * @param selection 
+     */
     public void setHouseSelected(House selection) {
         if(this.selectedHouse != null) {
             this.selectedHouse.unSelectHouse();
@@ -60,14 +115,22 @@ public class Player {
         this.selectedHouse = selection;
     }
     
+    /**
+     * Retourne la liste des maisons du joueur
+     * @return 
+     */
     public List<House> getHouses() {
         return houses;
     }
 
+    /**
+     * Setter de la liste de maisons
+     * @param houses 
+     */
     public void setHouses(List<House> houses) {
         this.houses = houses;
     }
-
+    
     @Override
     public String toString() {
         return name;
